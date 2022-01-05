bcf8ec8df8ceaef89871cf8710b0b7ee5e3fdc3a
Gordon Child <gordon@gordonchild.com>
2022-01-05 14:54:32 -0600

    removed some node commands as they are handled in omf

--- fishconfig/conf.d/mcfly.fish
+++ fishconfig/conf.d/mcfly.fish
@@ -1,9 +1,9 @@
 
-if type -q mcfly
-  set -gx MCFLY_KEY_SCHEME vim
-  set -gx MCFLY_FUZZY 3
+#if type -q mcfly
+#  set -gx MCFLY_KEY_SCHEME vim
+#  set -gx MCFLY_FUZZY 3
 
 
-  mcfly init fish | source
-end
+#  mcfly init fish | source
+#end
 
--- fishconfig/config.fish
+++ fishconfig/config.fish
@@ -1,5 +1,12 @@
 # ~/.config/fish/config.fish
 
+if type -q /usr/local/bin/screenfetch
+  alias screenfetch="/usr/local/bin/screenfetch -D 'Mac OS X'"
+end
+
+if type -q /opt/homebrew/bin/screenfetch
+  alias screenfetch="/opt/homebrew/bin/screenfetch -D 'Mac OS X'"
+end
 
 
 
